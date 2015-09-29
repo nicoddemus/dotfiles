b21fa907191423b9be2e6cfab1a232c64010a716
Bruno Oliveira <nicoddemus@gmail.com>
2015-09-28 23:50:24 -0300

    Update .env.bat

--- .env.bat
+++ .env.bat
@@ -17,42 +17,42 @@ set .rb=ruby
 alias mu=python D:\Programming\cygwin\home\Nico\projects\mu-repo\mu.py
 alias vim=D:\Programming\Shared\vim-7.2\vim72\gvim.exe
 alias d=dir
-alias ss=py27 x:\ss\ss.py
+alias ss=x:\ss\.env27\Scripts\python.exe x:\ss\ss.py
 alias activate=x:\dotfiles\activate.bat
 alias deactivate=x:\dotfiles\deactivate.bat
 alias ac=activate
 alias de=deactivate
+alias makedoc=sphinx-build -b html . _build
 
-for %v in (26, 27, 32, 33, 34) (
+for %v in (26, 27, 32, 33, 34, 35) (
     alias py%v=D:\Programming\Python%v\python.exe
     alias pip%v=py%v -m pip
-    alias py.test%v=py%v D:\Programming\Python%v\Scripts\py.test-script.py 
     alias venv%v=d:\Programming\Python%v\Scripts\virtualenv.exe
 )
 
 alias pypy=d:\Programming\pypy\pypy.exe
-alias tox=d:\Programming\Python27\Scripts\tox.exe
-alias ec=D:\Programming\Shared\eclipse\eclipse.exe
-alias np=notepad++
+alias st="C:\Program Files\Sublime Text 2\sublime_text.exe"
+set EDITOR="C:\Program Files\Sublime Text 2\sublime_text.exe" 
 alias cit=x:\cit\cit.bat
 alias gitext=D:\Programming\GitExtensions\GitExtensions.exe
 alias x=start explorer
 alias pyc=D:\Programming\PyCharm\bin\pycharm.exe
 
-set PATH="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin";%PATH%
-set PATH="C:\Program Files\Java\jre7\bin";%PATH%
-set PATH="D:\Programming\CMake28\bin";%PATH%
-set PATH="D:\Programming\Vagrant\bin";%PATH%
-set PATH="D:\Programming\VirtualBox";%PATH%
-set PATH="D:\Programming\Ruby1.9\bin";%PATH%
+alias vcvars34="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+alias vcvars27="C:\Users\Bruno\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat"
+REM set PATH="C:\Program Files\Java\jre7\bin";%PATH%
+REM set PATH="D:\Programming\CMake28\bin";%PATH%
+REM set PATH="D:\Programming\Vagrant\bin";%PATH%
+REM set PATH="D:\Programming\VirtualBox";%PATH%
+REM set PATH="D:\Programming\Ruby1.9\bin";%PATH%
 set PATH="d:\Programming\mkvtoolnix";%PATH%
 if exist call D:\Programming\DevKit\devkitvars.bat
 
 REM set PYTHONHOME=D:\Programming\Python27
 REM set PYTHONPATH=D:\Programming\Python27\dlls;D:\Programming\Python27\lib;D:\Programming\Python27\lib\site-packages
 REM set PATH=D:\Programming\Python27;D:\Programming\Python27\Scripts;%PATH%
-set PATH=x:\.rbenv\bin;%PATH%
-set TERM=msys
+REM set PATH=x:\.rbenv\bin;%PATH%
+REM set TERM=msys
 
 SET "VS90COMNTOOLS=%VS100COMNTOOLS%"
 
@@ -69,4 +69,5 @@ set INCLUDE=x:\boost_1_55_0;%INCLUDE%
 
 set LIBPATH=x:\gtest-1.7.0\Release;%LIBPATH%
 
-set QT_QPA_PLATFORM_PLUGIN_PATH=x:\qutebrowser\.venv\Lib\site-packages\PyQt5\plugins\platforms
+set QT_QPA_PLATFORM_PLUGIN_PATH=d:\Programming\Python34\Lib\site-packages\PyQt5\plugins\platforms\
+alias qpa=set QT_QPA_PLATFORM_PLUGIN_PATH=%%1\Lib\site-packages\PyQt5\plugins\platforms
