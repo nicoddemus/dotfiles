813ac119808e6800a09f1cb61ee75e556e4b5d0b
Bruno Oliveira <nicoddemus@gmail.com>
2017-07-24 21:24:37 -0300

    Add fetch-fork

--- .env.bat
+++ .env.bat
@@ -11,7 +11,7 @@ set bright_cyan=$e[1;36;m
 set bright_red=$e[1;31;m
 set white_on_red=$e[37;41;m
 set grey=$e[1;30;m
- 
+
 set HOME=x:\
 set .rb=ruby
 alias mu=python D:\Programming\cygwin\home\Nico\projects\mu-repo\mu.py
@@ -29,6 +29,8 @@ for %v in (26, 27, 32, 33, 34, 35) (
     alias venv%v=d:\Programming\Python%v\Scripts\virtualenv.exe
 )
 
+alias venv36=py -3.6 -m venv
+
 alias st="C:\Program Files\Sublime Text 2\sublime_text.exe"
 alias cit=x:\cit\cit.bat
 alias gitext=D:\Programming\GitExtensions\GitExtensions.exe
@@ -36,6 +38,7 @@ alias x=start explorer
 alias pyc=C:\PyCharm\bin\pycharm.exe
 
 alias vcvars34="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+alias vcvars35="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 alias vcvars27="C:\Users\Bruno\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat"
 set PATH=D:\Programming\Hub;%PATH%
 REM set PATH="C:\Program Files\Java\jre7\bin";%PATH%
@@ -62,10 +65,10 @@ REM TODO dashboard
 set TODO_DASHBOARD_GIT_URL=https://eden.esss.com.br/stash
 set TODO_DASHBOARD_PROJECTS=ESSS
 
-set INCLUDE=x:\gtest-1.7.0\include;%INCLUDE%
+set INCLUDE=X:\googletest-release-1.8.0\googletest\include;%INCLUDE%
 set INCLUDE=x:\boost_1_55_0;%INCLUDE%
 
-set LIBPATH=x:\gtest-1.7.0\Release;%LIBPATH%
+set LIBPATH=X:\googletest-release-1.8.0\Debug;%LIBPATH%
 
 :: set QT_QPA_PLATFORM_PLUGIN_PATH=d:\Programming\Python34\Lib\site-packages\PyQt5\plugins\platforms\
 :: alias qpa=set QT_QPA_PLATFORM_PLUGIN_PATH=%%1\Lib\site-packages\PyQt5\plugins\platforms
